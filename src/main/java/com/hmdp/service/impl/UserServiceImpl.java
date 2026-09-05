1062dd8c032955d4032cf1c6789c3e61ad13658a
itcast <hello@itcast.cn>
2026-09-05 20:03:08 +0800

    基于session实现用户登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -15,6 +25,46 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误");
+        }
+        String code = RandomUtil.randomNumbers(6);
+        session.setAttribute(phone, code);
+        log.debug("发送验证码：{}", code);
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号格式错误");
+        }
+        String code = loginForm.getCode();
+        Object sessionCode = session.getAttribute(phone);
+        if(sessionCode == null || !sessionCode.toString().equals(code)){
+            return Result.fail("验证码错误");
+        }
+        User user = query().eq("phone", phone).one();
+        if(user == null){
+            user=createUserWithPhone(phone);
+        }
+        UserDTO userDTO=new UserDTO();
+        BeanUtils.copyProperties(user, userDTO);
+        session.setAttribute("user", userDTO);
+        return Result.ok();
+
+    }
+    private User createUserWithPhone(String phone){
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        save(user);
+        return user;
+    }
 }
