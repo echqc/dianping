d96e8b10bbeb6c3acf17d0eb33794c6c89a85c0f
itcast <hello@itcast.cn>
2026-09-08 21:17:30 +0800

    redis实现全局唯一ID

--- src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -1,4 +1,32 @@
 package com.hmdp.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+@Component
 public class RedisIdWorker {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    private static final long BEGIN_TIMESTAMP=1767225600L;
+    private static final int COUNT_BITS = 32;
+    public long nextId(String keyPrefix){
+        //生成时间戳
+        long nowSecond = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC);
+        long timeStamp=nowSecond-BEGIN_TIMESTAMP;
+        //生成序列号
+        String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+        // 3.拼接并返回
+        return timeStamp << COUNT_BITS | count;
+    }
+    public static void main(String[] args) {
+        LocalDateTime time=LocalDateTime.of(2026,1,1,0,0,0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(second);
+
+    }
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -4,10 +4,15 @@ import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
@@ -16,12 +21,34 @@ class HmDianPingApplicationTests {
     private ShopServiceImpl shopService;
     @Autowired
     private CacheClient cacheClient;
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+    private ExecutorService es = Executors.newFixedThreadPool (500);
 
     @Test
     void testSaveShop() throws InterruptedException {
         Shop shop=shopService.getById(1L);
         cacheClient.setWithLogicalExpire(RedisConstants.CACHE_SHOP_KEY+1L,shop,10L, TimeUnit.SECONDS);
 
+    }
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
     }
 
 
