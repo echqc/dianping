506ab8d15ca5b7d00db32cf542ee30b90dac97ec
itcast <hello@itcast.cn>
2026-09-13 23:41:20 +0800

    优化点赞功能并实现点赞排行榜

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,8 +1,12 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
@@ -17,7 +21,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -63,11 +70,18 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     }
 
     private void isBlogLiked(Blog blog) {
+        UserDTO user=UserHolder.getUser();
+        if(user == null){
+            //用户未登录,无需查询是否点赞过
+            return;
+        }
         Long userId = UserHolder.getUser().getId();
         String key = RedisConstants.BLOG_LIKED_KEY+blog.getId();
-        Boolean isLiked = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
-        blog.setIsLike(BooleanUtil.isTrue(isLiked));
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        blog.setIsLike(score != null);
     }
+
+
 
     @Override
     public Result likeBlog(Long id) {
@@ -75,14 +89,14 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         Long userId = UserHolder.getUser().getId();
         //判断用户是否点赞过
         String key = RedisConstants.BLOG_LIKED_KEY + id;
-        Boolean isLiked = stringRedisTemplate.opsForSet().isMember(key, userId.toString());
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
         //没点赞过
-        if (BooleanUtil.isFalse(isLiked)) {
+        if (score == null) {
             // 点赞
             boolean isSuccess = update().setSql("liked = liked + 1").eq("id", id).update();
             //更新成功,保存用户到redis
             if (isSuccess) {
-                stringRedisTemplate.opsForSet().add(key, userId.toString());
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
             }
         }
         //点赞过
@@ -91,12 +105,34 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
             boolean isSuccess = update().setSql("liked = liked - 1").eq("id", id).update();
             //更新成功,删除用户从redis
             if (isSuccess) {
-                stringRedisTemplate.opsForSet().remove(key, userId.toString());
+                stringRedisTemplate.opsForZSet().remove(key, userId.toString());
             }
 
         }
         return Result.ok();
 
+    }
+
+    @Override
+    public Result queryBlogLikes(Long id) {
+        // 查询top5的点赞用户 zrange key 0 4
+        String key = RedisConstants.BLOG_LIKED_KEY + id;
+        Set<String> top5 = stringRedisTemplate.opsForZSet().range(key, 0, 4);
+        // 判断top5是否为空
+        if (CollUtil.isEmpty(top5)) {
+            return Result.ok(Collections.emptyList());
+        }
+        // 解析出其中的用户id
+        List<Long> userIds = top5.stream().map(Long::valueOf).collect(Collectors.toList());
+        String userIdsStr = StrUtil.join(",", userIds);	// 每个userId已逗号分隔
+        // 根据用户id查询用户，并转为UserVo集合 WHERE id IN (6, 2, 1) ORDER BY FIELD(id, 6, 2, 1) FIELD函数用于根据指定的顺序对结果进行排序
+        List<UserDTO> userVoList = userService.query()
+                .in("id", userIds).last("ORDER BY FIELD(id, " + userIdsStr + ")").list()
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        // 返回UserVo集合
+        return Result.ok(userVoList);
     }
 
     private void queryBlogUser(Blog blog) {
